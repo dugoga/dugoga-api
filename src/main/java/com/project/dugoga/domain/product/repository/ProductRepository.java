54a61699556c7a19ca7b37bde0a1f53811513c1e
jsh9057 <jsh9057@gmail.com>
2026-02-27 12:49:45 +0900

    add(#3): 상품(Product) 도메인 및 Entity, Repository 추가
    - Store Entity 추가
    - ProductRepository 추가

---
+++ src/main/java/com/project/dugoga/domain/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.project.dugoga.domain.product.repository;
+
+import com.project.dugoga.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
