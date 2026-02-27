5ea15edcf4d846b2017ef4d90d2303a6c954a6bd
hjk2132 <xiest@naver.com>
2026-02-27 17:50:58 +0900

    refactor : dev 동기화

--- src/main/java/com/project/dugoga/domain/user/entity/User.java
+++ src/main/java/com/project/dugoga/domain/user/entity/User.java
@@ -1,8 +1,8 @@
 package com.project.dugoga.domain.user.entity;
 
+import com.project.dugoga.domain.user.entity.enums.UserRoleEnum;
 import com.project.dugoga.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -22,23 +22,18 @@ public class User extends BaseEntity {
     private String email;
 
     @Column(nullable = false)
-    @Pattern(regexp = "^[A-Za-z0-9@$!%*?&#]{5,15}$", message = "비밀번호 형식이 올바르지 않습니다.")
     private String password;
 
-    @Column(nullable = false, length = 10)
+    @Column(nullable = false)
     @Size(min = 4, max = 10)
-    @Pattern(regexp = "^[A-Za-z가-힣]{4,10}$", message = "이름 형식이 올바르지 않습니다.")
     private String name;
 
     @Column(nullable = false, unique = true)
-    @Pattern(regexp = "^[A-Za-z0-9가-힣]{1,10}$", message = "닉네임 형식이 올바르지 않습니다.")
     private String nickname;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum userRole;
-
-    private boolean isDeleted = false;
 
     @Builder(access = AccessLevel.PRIVATE)
     private User(
@@ -65,7 +60,7 @@ public class User extends BaseEntity {
                 .build();
     }
 
-    public void softDelete() {
-        this.isDeleted = true;
+    public void withdraw(Long userId) {
+        softDelete(userId);
     }
 }
