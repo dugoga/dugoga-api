3cc425affb0b8efa193f936f4b619f1fd0bad4ea
gsemily <gsemily22@gmail.com>
2026-02-28 19:05:22 +0900

    feat: RedisConfig에 StringRedisTemplate 메서드 추가

--- src/main/java/com/project/dugoga/global/config/RedisConfig.java
+++ src/main/java/com/project/dugoga/global/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -26,6 +27,13 @@ public class RedisConfig {
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
+    // 문자열 저장을 위한 StringRedisTemplate
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory){
+        return new StringRedisTemplate(redisConnectionFactory);
+    }
+
+    // 객체(JSON) 저장을 위한 RedisTemplate
     @Bean
     public RedisTemplate<String, Object> redisTemplate(
             RedisConnectionFactory connectionFactory
