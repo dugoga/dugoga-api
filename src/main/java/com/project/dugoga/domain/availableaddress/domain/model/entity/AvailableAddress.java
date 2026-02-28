79ebde180889edcf253f2f164fccc9b7d6474bb3
oni128 <lyw283828@gmail.com>
2026-02-28 18:33:09 +0900

    refactor: 도메인 세팅

--- src/main/java/com/project/dugoga/domain/availableaddress/domain/model/entity/AvailableAddress.java
+++ src/main/java/com/project/dugoga/domain/availableaddress/domain/model/entity/AvailableAddress.java
@@ -3,25 +3,35 @@ package com.project.dugoga.domain.availableaddress.domain.model.entity;
 import com.project.dugoga.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "p_available_address")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AvailableAddress extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false)
-    private String region_1depth_name;
+    private String region1DepthName;
 
     @Column(nullable = false)
-    private String region_2depth_name;
+    private String region2DepthName;
 
     @Column(nullable = false)
-    private String region_3depth_name;
+    private String region3DepthName;
 
-
+    private AvailableAddress(String region1DepthName, String region2DepthName, String region3DepthName) {
+        this.region1DepthName = region1DepthName;
+        this.region2DepthName = region2DepthName;
+        this.region3DepthName = region3DepthName;
+    }
 }
