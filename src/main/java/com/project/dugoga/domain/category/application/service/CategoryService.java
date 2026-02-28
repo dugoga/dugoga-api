f8b01167add080869e9d825f8ff67d602cb93955
oni128 <lyw283828@gmail.com>
2026-03-01 01:36:01 +0900

    refactor(category) : 디버그 로그 제거

--- src/main/java/com/project/dugoga/domain/category/application/service/CategoryService.java
+++ src/main/java/com/project/dugoga/domain/category/application/service/CategoryService.java
@@ -10,13 +10,11 @@ import com.project.dugoga.domain.category.domain.repository.CategoryRepository;
 import com.project.dugoga.global.exception.BusinessException;
 import com.project.dugoga.global.exception.ErrorCode;
 import java.util.UUID;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Slf4j
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
