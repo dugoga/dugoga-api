f825619365f18e33338f88ec7cb58eb019ed25ad
gsemily <gsemily22@gmail.com>
2026-03-05 12:00:18 +0900

    refactor: 불필요한 라이브러리 삭제

--- src/main/java/com/project/dugoga/global/config/properties/RefreshTokenProperties.java
+++ src/main/java/com/project/dugoga/global/config/properties/RefreshTokenProperties.java
@@ -2,7 +2,6 @@ package com.project.dugoga.global.config.properties;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
