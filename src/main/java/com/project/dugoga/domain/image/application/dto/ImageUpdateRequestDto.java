9e654711002da9f13fbaf82b08d433fb0f14deed
hjk2132 <xiest@naver.com>
2026-03-06 12:30:57 +0900

    fix(image): ArgsConstructor 어노테이션 추가

--- src/main/java/com/project/dugoga/domain/image/application/dto/ImageUpdateRequestDto.java
+++ src/main/java/com/project/dugoga/domain/image/application/dto/ImageUpdateRequestDto.java
@@ -3,9 +3,13 @@ package com.project.dugoga.domain.image.application.dto;
 import com.project.dugoga.domain.image.domain.model.enums.FileType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageUpdateRequestDto {
     @NotBlank
     private String fileName;
