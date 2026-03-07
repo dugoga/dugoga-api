50a39558e4c14096e155d6f4ec68e16847727d2d
oni128 <lyw283828@gmail.com>
2026-03-08 01:22:01 +0900

    deploy: EC2 docker compose 기반 서비스 배포 및 환경설정

---
+++ src/main/java/com/project/dugoga/domain/aws/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.project.dugoga.domain.aws;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> HealthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+
+}
