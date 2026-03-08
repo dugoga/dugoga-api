e07892f9c5c7202bfdbbe86e6dae9cba19a3f87e
jsh9057 <jsh9057@gmail.com>
2026-03-08 17:59:34 +0900

    refactor(#87): Product Repository 분리
    - QueryDsl 도입으로 Repository 관리의 용이성을위해 분리
    - StoreJpaRepository에 null 비교를 '=' 잘못비교한 부분 수정

--- src/main/java/com/project/dugoga/domain/store/infrastructure/repository/StoreJpaRepository.java
+++ src/main/java/com/project/dugoga/domain/store/infrastructure/repository/StoreJpaRepository.java
@@ -1,8 +1,6 @@
 package com.project.dugoga.domain.store.infrastructure.repository;
 
 import com.project.dugoga.domain.store.domain.model.entity.Store;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,8 +12,8 @@ import java.util.UUID;
 
 public interface StoreJpaRepository extends JpaRepository<Store, UUID> {
 
-    @Query("select s from Store s join fetch s.user join fetch s.category where s.id = :storeId and s.deletedAt = null")
-    Optional<Store> findByIdWithDetailsAndDeletedAtIsNull(@Param("stoerId") UUID storeId);
+    @Query("select s from Store s join fetch s.user join fetch s.category where s.id = :storeId and s.deletedAt is null")
+    Optional<Store> findByIdWithDetailsAndDeletedAtIsNull(@Param("storeId") UUID storeId);
 
     Optional<Store> findByIdAndDeletedAtIsNull(UUID storeId);
 
