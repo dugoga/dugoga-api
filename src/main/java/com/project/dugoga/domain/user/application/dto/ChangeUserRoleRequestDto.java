a484bb16b5fa483e979bd5fbdebcca19456312cb
gsemily <gsemily22@gmail.com>
2026-03-09 00:59:00 +0900

    ffeat: 유저 권한 변경 request DTO 작성

---
+++ src/main/java/com/project/dugoga/domain/user/application/dto/ChangeUserRoleRequestDto.java
@@ -0,0 +1,14 @@
+package com.project.dugoga.domain.user.application.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangeUserRoleRequestDto {
+    @NotBlank(message = "권한을 입력해주세요.")
+    private String userRole;
+}
