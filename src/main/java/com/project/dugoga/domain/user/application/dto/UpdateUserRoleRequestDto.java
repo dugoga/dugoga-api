590331c869f8311f14b719d1c5a6f57ebd8c3de4
gsemily <gsemily22@gmail.com>
2026-03-09 01:21:52 +0900

    feat: 회원 유형 검증 메서드 추가

--- src/main/java/com/project/dugoga/domain/user/application/dto/UpdateUserRoleRequestDto.java
+++ src/main/java/com/project/dugoga/domain/user/application/dto/UpdateUserRoleRequestDto.java
@@ -1,5 +1,7 @@
 package com.project.dugoga.domain.user.application.dto;
 
+import com.project.dugoga.domain.user.domain.model.enums.UserRoleEnum;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateUserRoleRequestDto {
-    @NotBlank(message = "권한을 입력해주세요.")
-    private String userRole;
+    @NotBlank(message = "회원 유형을 입력해주세요.")
+    private UserRoleEnum userRole;
+
+    @AssertTrue(message = "회원 유형은 CUSTOMER, OWNER, MANAGER, MASTER 중에서만 가능합니다.")
+    public boolean isValidUserRole() {
+        return userRole == UserRoleEnum.CUSTOMER
+                || userRole == UserRoleEnum.OWNER
+                || userRole == UserRoleEnum.MANAGER
+                || userRole == UserRoleEnum.MASTER;
+    }
 }
