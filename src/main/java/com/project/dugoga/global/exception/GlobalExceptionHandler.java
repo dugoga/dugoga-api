fb45809bd404b583f270291af8b69db95aaad75a
oni128 <lyw283828@gmail.com>
2026-03-08 14:24:56 +0900

    fix(category) : AuthorizationDeniedException 전역 예외 처리 추가(#98)

--- src/main/java/com/project/dugoga/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/dugoga/global/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
@@ -76,6 +77,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(httpStatus)
                 .body(ErrorResponse.of(ex.getMessage()));
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ErrorResponse<String>> handleAccessDenied(Exception ex) {
+        ErrorCode errorCode = ErrorCode.FORBIDDEN;
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode.getDefaultMessage()));
     }
 
     @ExceptionHandler(Exception.class)
