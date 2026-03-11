d4701d2f23398ca441ef22ac83da70a3c136ecb7
soo <dlgustn96@gmail.com>
2026-03-11 12:48:03 +0900

    refactor(order): 주문 레포지토리 패키지 구조 변경 (#173)

--- src/main/java/com/project/dugoga/domain/order/infrastructure/repository/OrderProductJpaRepository.java
+++ src/main/java/com/project/dugoga/domain/order/infrastructure/repository/OrderProductJpaRepository.java
@@ -1,11 +1,12 @@
-package com.project.dugoga.domain.order.domain.repository;
+package com.project.dugoga.domain.order.infrastructure.repository;
 
 import com.project.dugoga.domain.order.domain.model.entity.OrderProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-public interface OrderProductRepository {
+public interface OrderProductJpaRepository extends JpaRepository<OrderProduct, UUID> {
     List<OrderProduct> findAllByOrder_IdIn(Collection<UUID> orderIds);
 }
