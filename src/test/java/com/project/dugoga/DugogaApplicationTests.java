63383486a53a48f7a910791f7062b9a1331c4d1f
hjk2132 <xiest@naver.com>
2026-03-11 13:12:40 +0900

    refactor: add activeProfiles annotation

--- src/test/java/com/project/dugoga/DugogaApplicationTests.java
+++ src/test/java/com/project/dugoga/DugogaApplicationTests.java
@@ -1,46 +1,15 @@
 package com.project.dugoga;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-
-import com.project.dugoga.domain.availableaddress.domain.model.entity.AvailableAddress;
-import com.project.dugoga.domain.bookmark.application.dto.BookmarkCreateResponseDto;
-import com.project.dugoga.domain.bookmark.application.dto.BookmarkListResponseDto;
-import com.project.dugoga.domain.bookmark.application.dto.BookmarkUpdateResponseDto;
-import com.project.dugoga.domain.bookmark.application.dto.BookmarkVisibilityUpdateRequestDto;
-import com.project.dugoga.domain.bookmark.application.service.BookmarkService;
-import com.project.dugoga.domain.bookmark.domain.model.entity.Bookmark;
-import com.project.dugoga.domain.bookmark.domain.repository.BookmarkRepository;
-import com.project.dugoga.domain.category.domain.model.entity.Category;
-import com.project.dugoga.domain.store.domain.model.entity.Store;
-import com.project.dugoga.domain.store.domain.repository.StoreRepository;
-import com.project.dugoga.domain.user.domain.model.entity.User;
-import com.project.dugoga.domain.user.domain.model.enums.UserRoleEnum;
-import com.project.dugoga.domain.user.domain.repository.UserRepository;
-import com.project.dugoga.global.exception.BusinessException;
-import com.project.dugoga.global.exception.ErrorCode;
-import java.time.LocalTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@ActiveProfiles
 class DugogaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
 }
